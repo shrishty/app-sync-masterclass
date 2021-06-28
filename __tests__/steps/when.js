f9c7cb42865d8f278df3eefed7616604b296aaf5
Shrishty Chandra <shrishty@google.com>
2021-06-28 07:54:58 +0530

    Added tests for like and unlike

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -3,294 +3,354 @@ const AWS = require('aws-sdk')
 const fs = require('fs')
 const velocityMapper = require('amplify-appsync-simulator/lib/velocity/value-mapper/mapper')
 const velocityTemplate = require('amplify-velocity-template')
-const GraphQL = require('../lib/graphql')
+const { GraphQL, registerFragment } = require('../lib/graphql')
+
+const myProfileFragment = `
+fragment myProfileFields on MyProfile {
+  id
+  name
+  screenName
+  imageUrl
+  backgroundImageUrl
+  bio
+  location
+  website
+  birthdate
+  createdAt
+  followersCount
+  followingCount
+  tweetsCount
+  likesCounts
+}
+`
+
+const otherProfileFragment = `
+fragment otherProfileFields on OtherProfile {
+  id
+  name
+  screenName
+  imageUrl
+  backgroundImageUrl
+  bio
+  location
+  website
+  birthdate
+  createdAt
+  followersCount
+  followingCount
+  tweetsCount
+  likesCounts
+}
+`
+
+const iProfileFragment = `
+fragment iProfileFields on IProfile {
+  ... on MyProfile {
+    ... myProfileFields
+  }
+
+  ... on OtherProfile {
+    ... otherProfileFields
+  }
+}
+`
+
+const tweetFragment = `
+fragment tweetFields on Tweet {
+  id
+  profile {
+    ... iProfileFields
+  }
+  createdAt
+  text
+  replies
+  likes
+  retweets
+  liked
+}
+`
+
+const iTweetFragment = `
+fragment iTweetFields on ITweet {
+  ... on Tweet {
+    ... tweetFields
+  }
+}
+`
+
+registerFragment('myProfileFields', myProfileFragment)
+registerFragment('otherProfileFields', otherProfileFragment)
+registerFragment('iProfileFields', iProfileFragment)
+registerFragment('tweetFields', tweetFragment)
+registerFragment('iTweetFields', iTweetFragment)
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
-    const handler = require('../../functions/confirm-user-signup').handler
+  const handler = require('../../functions/confirm-user-signup').handler
 
-    const context = {}
-    const event = {
-        "version": "1",
-        "region": process.env.AWS_REGION,
-        "userPoolId": process.env.COGNITO_USER_POOL_ID,
-        "userName": username,
-        "triggerSource": "PostConfirmation_ConfirmSignUp",
-        "request": {
-            "userAttributes": {
-                "sub": username,
-                "cognito:email_alias": email,
-                "cognito:user_status": "CONFIRMED",
-                "email_verified": "false",
-                "name": name,
-                "email": email
-            }
-        },
-        "response": {}
-    }
+  const context = {}
+  const event = {
+    "version": "1",
+    "region": process.env.AWS_REGION,
+    "userPoolId": process.env.COGNITO_USER_POOL_ID,
+    "userName": username,
+    "triggerSource": "PostConfirmation_ConfirmSignUp",
+    "request": {
+      "userAttributes": {
+        "sub": username,
+        "cognito:email_alias": email,
+        "cognito:user_status": "CONFIRMED",
+        "email_verified": "false",
+        "name": name,
+        "email": email
+      }
+    },
+    "response": {}
+  }
 
-    await handler(event, context)
-}
-
-const a_user_signs_up = async (password, name, email) => {
-    const cognito = new AWS.CognitoIdentityServiceProvider()
-
-    const userPoolId = process.env.COGNITO_USER_POOL_ID
-    const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID
-
-    const signUpResp = await cognito.signUp({
-        ClientId: clientId,
-        Username: email,
-        Password: password,
-        UserAttributes: [
-            { Name: 'name', Value: name }
-        ]
-    }).promise()
-
-    const username = signUpResp.UserSub
-    console.log(`[${email}] - user has signed up [${username}]`)
-
-    await cognito.adminConfirmSignUp({
-        UserPoolId: userPoolId,
-        Username: username
-    }).promise()
-
-    console.log(`[${email}] - confirmed sign up`)
-
-    return {
-        username,
-        name,
-        email
-    }
-}
-
-const we_invoke_an_appsync_template = (templatePath, context) => {
-    const template = fs.readFileSync(templatePath, { encoding: 'utf-8' })
-    const ast = velocityTemplate.parse(template)
-    const compiler = new velocityTemplate.Compile(ast, {
-        valueMapper: velocityMapper.map,
-        escape: false
-    })
-    return JSON.parse(compiler.render(context))
-}
-
-const a_user_calls_getMyProfile = async (user) => {
-    const getMyProfile = `query getMyProfile {
-        getMyProfile {
-            backgroundImageUrl
-            bio
-            birthdate
-            createdAt
-            followersCount
-            followingCount
-            id
-            imageUrl
-            likesCount
-            location
-            name
-            screenName
-            tweetsCount
-            website
-        }
-    }`
-
-    const data = await GraphQL(process.env.API_URL, getMyProfile, {}, user.accessToken)
-    const profile = data.getMyProfile
-
-    console.log(`[${user.username}] - fetched profile`)
-    return profile
-}
-
-const a_user_calls_editMyProfile = async (user, input) => {
-    const editMyProfile = `mutation editMyProfile($input: ProfileInput!) {
-        editMyProfile(newProfile: $input) {
-            backgroundImageUrl
-            bio
-            birthdate
-            createdAt
-            followersCount
-            followingCount
-            id
-            imageUrl
-            likesCount
-            location
-            name
-            screenName
-            tweetsCount
-            website
-        }
-    }`
-
-    const variables = {
-        input
-    }
-
-    const data = await GraphQL(process.env.API_URL, editMyProfile, variables, user.accessToken)
-    const profile = data.editMyProfile
-
-    console.log(`[${user.username}] - edited profile`)
-    return profile
+  await handler(event, context)
 }
 
 const we_invoke_getImageUploadUrl = async (username, extension, contentType) => {
-    const handler = require('../../functions/get-upload-url').handler
+  const handler = require('../../functions/get-upload-url').handler
 
-    const context = {}
-    const event = {
-        identity: {
-            username
-        },
-        arguments: {
-            extension,
-            contentType
-        }
+  const context = {}
+  const event = {
+    identity: {
+      username
+    },
+    arguments: {
+      extension,
+      contentType
     }
+  }
 
-    return await handler(event, context)
+  return await handler(event, context)
 }
 
 const we_invoke_tweet = async (username, text) => {
-    const handler = require('../../functions/tweet').handler
+  const handler = require('../../functions/tweet').handler
 
-    const context = {}
-    const event = {
-        identity: {
-            username
-        },
-        arguments: {
-            text
-        }
+  const context = {}
+  const event = {
+    identity: {
+      username
+    },
+    arguments: {
+      text
     }
+  }
 
-    return await handler(event, context)
+  return await handler(event, context)
+}
+
+const a_user_signs_up = async (password, name, email) => {
+  const cognito = new AWS.CognitoIdentityServiceProvider()
+
+  const userPoolId = process.env.COGNITO_USER_POOL_ID
+  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID
+
+  const signUpResp = await cognito.signUp({
+    ClientId: clientId,
+    Username: email,
+    Password: password,
+    UserAttributes: [
+      { Name: 'name', Value: name }
+    ]
+  }).promise()
+
+  const username = signUpResp.UserSub
+  console.log(`[${email}] - user has signed up [${username}]`)
+
+  await cognito.adminConfirmSignUp({
+    UserPoolId: userPoolId,
+    Username: username
+  }).promise()
+
+  console.log(`[${email}] - confirmed sign up`)
+
+  return {
+    username,
+    name,
+    email
+  }
+}
+
+const we_invoke_an_appsync_template = (templatePath, context) => {
+  const template = fs.readFileSync(templatePath, { encoding: 'utf-8' })
+  const ast = velocityTemplate.parse(template)
+  const compiler = new velocityTemplate.Compile(ast, {
+    valueMapper: velocityMapper.map,
+    escape: false
+  })
+  return JSON.parse(compiler.render(context))
+}
+
+const a_user_calls_getMyProfile = async (user) => {
+  const getMyProfile = `query getMyProfile {
+    getMyProfile {
+      ... myProfileFields
+    }
+  }`
+
+  const data = await GraphQL(process.env.API_URL, getMyProfile, {}, user.accessToken)
+  const profile = data.getMyProfile
+
+  console.log(`[${user.username}] - fetched profile`)
+
+  return profile
+}
+
+const a_user_calls_editMyProfile = async (user, input) => {
+  const editMyProfile = `mutation editMyProfile($input: ProfileInput!) {
+    editMyProfile(newProfile: $input) {
+      ... myProfileFields
+    }
+  }`
+  const variables = {
+    input
+  }
+
+  const data = await GraphQL(process.env.API_URL, editMyProfile, variables, user.accessToken)
+  const profile = data.editMyProfile
+
+  console.log(`[${user.username}] - edited profile`)
+
+  return profile
 }
 
 const a_user_calls_getImageUploadUrl = async (user, extension, contentType) => {
-    const getImageUploadUrl = `query getImageUploadUrl($extension: String, $contentType: String) {
-        getImageUploadUrl(extension: $extension, contentType: $contentType)
-    }`
+  const getImageUploadUrl = `query getImageUploadUrl($extension: String, $contentType: String) {
+    getImageUploadUrl(extension: $extension, contentType: $contentType)
+  }`
+  const variables = {
+    extension,
+    contentType
+  }
 
-    const variables = {
-        extension,
-        contentType
-    }
+  const data = await GraphQL(process.env.API_URL, getImageUploadUrl, variables, user.accessToken)
+  const url = data.getImageUploadUrl
 
-    const data = await GraphQL(process.env.API_URL, getImageUploadUrl, variables, user.accessToken)
-    const url = data.getImageUploadUrl
+  console.log(`[${user.username}] - got image upload url`)
 
-    console.log(`[${user.username}] - got image upload url [${url}]`)
-    return url
+  return url
 }
 
 const a_user_calls_tweet = async (user, text) => {
-    const tweet = `mutation tweet($text: String!) {
-        tweet(text: $text) {
-            id
-            createdAt
-            profile {
-                id
-                name
-                screenName
-            }
-            text
-            replies
-            likes
-            retweets
-            liked
-        }
-    }`
-
-    const variables = {
-        text
+  const tweet = `mutation tweet($text: String!) {
+    tweet(text: $text) {
+      id
+      profile {
+        ... iProfileFields
+      }
+      createdAt
+      text
+      replies
+      likes
+      retweets
+      liked
     }
+  }`
+  const variables = {
+    text
+  }
 
-    const data = await GraphQL(process.env.API_URL, tweet, variables, user.accessToken)
-    const newtweet = data.tweet
+  const data = await GraphQL(process.env.API_URL, tweet, variables, user.accessToken)
+  const newTweet = data.tweet
 
-    console.log(`[${user.username}] - posted new tweet`)
-    return newtweet
+  console.log(`[${user.username}] - posted new tweet`)
+
+  return newTweet
 }
 
-const a_user_calls_getTweet = async (user, userId, limit, nextToken) => {
-    const getTweets = `query getTweets($userId: ID!, $limit: Int!, $nextToken: String) {
-        getTweets(userId: $userId, limit: $limit, nextToken: $nextToken) {
-            nextToken
-            tweets {
-                id
-                createdAt
-                profile {
-                    id
-                    name
-                    screenName
-                }
-
-                ... on Tweet {
-                    text
-                    replies
-                    likes
-                    retweets
-                }
-            }
-        }
-    }`
-
-    const variables = {
-        userId,
-        limit,
-        nextToken
+const a_user_calls_getTweets = async (user, userId, limit, nextToken) => {
+  const getTweets = `query getTweets($userId: ID!, $limit: Int!, $nextToken: String) {
+    getTweets(userId: $userId, limit: $limit, nextToken: $nextToken) {
+      nextToken
+      tweets {
+        ... iTweetFields
+      }
     }
+  }`
+  const variables = {
+    userId,
+    limit,
+    nextToken
+  }
 
-    const data = await GraphQL(process.env.API_URL, getTweets, variables, user.accessToken)
-    const tweets = data.getTweets
+  const data = await GraphQL(process.env.API_URL, getTweets, variables, user.accessToken)
+  const result = data.getTweets
 
-    console.log(`[${user.username}] - get tweet called`)
-    return tweets
+  console.log(`[${user.username}] - posted new tweet`)
+
+  return result
 }
 
 const a_user_calls_getMyTimeline = async (user, limit, nextToken) => {
-    const getMyTimeline = `query getMyTimeline($limit: Int!, $nextToken: String) {
-        getMyTimeline(limit: $limit, nextToken: $nextToken) {
-            nextToken
-            tweets {
-                id
-                createdAt
-                profile {
-                    id
-                    name
-                    screenName
-                }
-
-                ... on Tweet {
-                    text
-                    replies
-                    likes
-                    retweets
-                }
-            }
-        }
-    }`
-
-    const variables = {
-        limit,
-        nextToken
+  const getMyTimeline = `query getMyTimeline($limit: Int!, $nextToken: String) {
+    getMyTimeline(limit: $limit, nextToken: $nextToken) {
+      nextToken
+      tweets {
+        ... iTweetFields
+      }
     }
+  }`
+  const variables = {
+    limit,
+    nextToken
+  }
 
-    const data = await GraphQL(process.env.API_URL, getMyTimeline, variables, user.accessToken)
-    const result = data.getMyTimeline
+  const data = await GraphQL(process.env.API_URL, getMyTimeline, variables, user.accessToken)
+  const result = data.getMyTimeline
 
-    console.log(`[${user.username}] - fetched timeline`)
-    return result
+  console.log(`[${user.username}] - fetched timeline`)
+
+  return result
+}
+
+const a_user_calls_like = async (user, tweetId) => {
+  const like = `mutation like($tweetId: ID!) {
+    like(tweetId: $tweetId)
+  }`
+  const variables = {
+    tweetId
+  }
+
+  const data = await GraphQL(process.env.API_URL, like, variables, user.accessToken)
+  const result = data.like
+
+  console.log(`[${user.username}] - liked tweet [${tweetId}]`)
+
+  return result
+}
+
+const a_user_calls_unlike = async (user, tweetId) => {
+  const unlike = `mutation unlike($tweetId: ID!) {
+    unlike(tweetId: $tweetId)
+  }`
+  const variables = {
+    tweetId
+  }
+
+  const data = await GraphQL(process.env.API_URL, unlike, variables, user.accessToken)
+  const result = data.unlike
+
+  console.log(`[${user.username}] - unliked tweet [${tweetId}]`)
+
+  return result
 }
 
 module.exports = {
-    we_invoke_confirmUserSignup,
-    a_user_signs_up,
-    we_invoke_an_appsync_template,
-    a_user_calls_getMyProfile,
-    a_user_calls_editMyProfile,
-    we_invoke_getImageUploadUrl,
-    a_user_calls_getImageUploadUrl,
-    we_invoke_tweet,
-    a_user_calls_tweet,
-    a_user_calls_getTweet,
-    a_user_calls_getMyTimeline
+  we_invoke_confirmUserSignup,
+  we_invoke_getImageUploadUrl,
+  we_invoke_tweet,
+  a_user_signs_up,
+  we_invoke_an_appsync_template,
+  a_user_calls_getMyProfile,
+  a_user_calls_editMyProfile,
+  a_user_calls_getImageUploadUrl,
+  a_user_calls_tweet,
+  a_user_calls_getTweets,
+  a_user_calls_getMyTimeline,
+  a_user_calls_like,
+  a_user_calls_unlike
 }
--- __tests__/test_cases/e2e/tweeting.js
+++ __tests__/test_cases/e2e/tweeting.js
@@ -1,80 +1,120 @@
 require('dotenv').config()
 const given = require('../../steps/given')
-const when = require('../../steps/when')
 const then = require('../../steps/then')
+const when = require('../../steps/when')
 const chance = require('chance').Chance()
-const path = require('path')
 
 describe('Given an authenticated user', () => {
-    let user
+  let user
+  beforeAll(async () => {
+    user = await given.an_authenticated_user()
+  })
+
+  describe('When he sends a tweet', () => {
+    let tweet
+    const text = chance.string({ length: 16 })
     beforeAll(async () => {
-        user = await given.an_authenticated_user()
+      tweet = await when.a_user_calls_tweet(user, text)
     })
 
-    describe('when he sends a tweet', () => {
-        let tweet
-        const text = chance.string({ length: 16 })
+    it('Should return the new tweet', () => {
+      expect(tweet).toMatchObject({
+        text,
+        replies: 0,
+        likes: 0,
+        retweets: 0,
+        liked: false,
+      })
+    })
+
+    describe('When he calls getTweets', () => {
+      let tweets, nextToken
+      beforeAll(async () => {
+        const result = await when.a_user_calls_getTweets(user, user.username, 25)
+        tweets = result.tweets
+        nextToken = result.nextToken
+      })
+
+      it('He will see the new tweet in the tweets array', () => {
+        expect(nextToken).toBeNull()
+        expect(tweets.length).toEqual(1)
+        expect(tweets[0]).toEqual(tweet)
+      })
+  
+      it('He cannot ask for more than 25 tweets in a page', async () => {
+        await expect(when.a_user_calls_getTweets(user, user.username, 26))
+          .rejects
+          .toMatchObject({
+            message: expect.stringContaining('max limit is 25')
+          })
+      })
+    })
+
+    describe('When he calls getMyTimeline', () => {
+      let tweets, nextToken
+      beforeAll(async () => {
+        const result = await when.a_user_calls_getMyTimeline(user, 25)
+        tweets = result.tweets
+        nextToken = result.nextToken
+      })
+
+      it('He will see the new tweet in the tweets array', () => {
+        expect(nextToken).toBeNull()
+        expect(tweets.length).toEqual(1)
+        expect(tweets[0]).toEqual(tweet)
+      })
+  
+      it('He cannot ask for more than 25 tweets in a page', async () => {
+        await expect(when.a_user_calls_getMyTimeline(user, 26))
+          .rejects
+          .toMatchObject({
+            message: expect.stringContaining('max limit is 25')
+          })
+      })
+    })
+
+    describe('When he likes the tweet', () => {
+      beforeAll(async () => {
+        await when.a_user_calls_like(user, tweet.id)
+      })
+
+      it('Should see Tweet.liked as true', async () => {
+        const { tweets } = await when.a_user_calls_getMyTimeline(user, 25)
+
+        expect(tweets).toHaveLength(1)
+        expect(tweets[0].id).toEqual(tweet.id)
+        expect(tweets[0].liked).toEqual(true)
+      })
+
+      it('Should not be able to like the same tweet a second time', async () => {
+        await expect(() => when.a_user_calls_like(user, tweet.id))
+          .rejects
+          .toMatchObject({
+            message: expect.stringContaining('DynamoDB transaction error')
+          })
+      })
+
+      describe('When he unlikes the tweet', () => {
         beforeAll(async () => {
-            tweet = await when.a_user_calls_tweet(user, text)
-            console.log(`tweet =`, tweet)
+          await when.a_user_calls_unlike(user, tweet.id)
         })
 
-        it('Should return the new tweet', () => {
-            expect(tweet).toMatchObject({
-                text,
-                replies: 0,
-                likes: 0,
-                retweets: 0,
-                liked: false,
+        it('Should see Tweet.liked as false', async () => {
+          const { tweets } = await when.a_user_calls_getMyTimeline(user, 25)
+  
+          expect(tweets).toHaveLength(1)
+          expect(tweets[0].id).toEqual(tweet.id)
+          expect(tweets[0].liked).toEqual(false)
+        })
+  
+        it('Should not be able to unlike the same tweet a second time', async () => {
+          await expect(() => when.a_user_calls_unlike(user, tweet.id))
+            .rejects
+            .toMatchObject({
+              message: expect.stringContaining('DynamoDB transaction error')
             })
         })
-
-        describe('When he calls getTweets', () => {
-            let tweets, nextToken
-            beforeAll(async () => {
-                const result = await when.a_user_calls_getTweet(user, user.username, 25)
-                tweets = result.tweets
-                nextToken = result.nextToken
-            })
-
-
-            it('He will see the new tweet in the tweets array', () => {
-                expect(nextToken).toBeNull()
-                expect(tweets.length).toEqual(1)
-                expect(tweets[0]).toEqual(tweet)
-            })
-    
-            it('he can not ask for more than 25 tweets in a page', async () => {
-                await expect(when.a_user_calls_getTweet(user, user.username, 26))
-                    .rejects
-                    .toMatchObject({
-                        message: expect.stringContaining('max limit is 25')
-                })
-            })
-        })
-
-        describe('When he calls getMyTimeline', () => {
-            let tweets, nextToken
-            beforeAll(async () => {
-                const result = await when.a_user_calls_getMyTimeline(user, 25)
-                tweets = result.tweets
-                nextToken = result.nextToken
-            })
-
-
-            it('He will see the new tweet in the tweets array', () => {
-                expect(nextToken).toBeNull()
-                expect(tweets.length).toEqual(1)
-                expect(tweets[0]).toEqual(tweet)
-            })
-    
-            it('he can not ask for more than 25 tweets in a page', async () => {
-                await expect(when.a_user_calls_getMyTimeline(user, 26))
-                    .rejects
-                    .toMatchObject({
-                        message: expect.stringContaining('max limit is 25')
-                })
-            })
-        })
+      })
     })
+  })
 })
